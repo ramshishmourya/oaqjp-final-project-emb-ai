7028b44265c9f91f0772419ccbc96ba75f4bd0bd
Ramsish Mourya <ramshishmourya@gmail.com>
2026-06-13 12:30:06 -0400

    server changes

--- server.py
+++ server.py
@@ -19,3 +19,7 @@ def emotion_detector():
     dominant_emotion = emotions['dominant_emotion']
     return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger,disgust,fear,joy,sadness,dominant_emotion)
 
+
+
+if __name__ == "__main__" : 
+    app.run(host="0.0.0.0", port=5000) 
